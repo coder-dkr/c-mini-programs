0ef1797a23f7ae8761c88968c4c89938ed4b3d31
Dhruv Roy <dhruvakr9001@gmail.com>
2024-04-21 17:14:26 +0530

    added c in simple drive test

---
+++ drivetest.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+void drivetest();
+
+int main() {
+    drivetest();
+    return 0;
+}
+
+void drivetest(){
+    int age;
+    char opt;
+    printf("\nAre you Immortal? (y/n)\n");
+    scanf("%c",&opt);
+    if(opt == 'y'){
+        printf("You can definatelt drive sir..\n");
+    }
+    else if(opt == 'n'){
+        printf("Enter your age:\n");
+        scanf("%d",&age);
+        if((age < 18) || (age > 85)){
+            printf("You cannot drive...\n");
+        }
+        else{
+            printf("You can drive...\n");
+        }
+        
+    }
+    else{
+        printf("Invalid input.try again\n");
+        drivetest();
+    }
+}
