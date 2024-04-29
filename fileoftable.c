f04e871c8bc0e473b98f6de71867d5d0bf44e2ba
Dhruv Roy <dhruvakr9001@gmail.com>
2024-04-29 15:53:19 +0530

    table writing in file

---
+++ fileoftable.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    FILE *ptr;
+    int num;
+    ptr =fopen("table.txt","w");
+    printf("Enter number you want table of in table.txt: ");
+    scanf("%d",&num);
+
+    for(int i = 0; i<10;i++){
+
+        fprintf(ptr,"%d X %d = %d\n",num,(i+1),num*(i+1));
+    }
+    if(ptr == NULL){
+        printf("unsuccessful");
+    }
+    else{
+        printf("Printed successfully");
+    }
+
+    return 0;
+}
