78e5bec6c1702bdde55ef69946e0d4acfb341d72
Dhruv Roy <dhruvakr9001@gmail.com>
2024-04-29 15:36:57 +0530

    file handling basics

---
+++ filebasics.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    FILE *ptr;
+
+    //reading a file charcater by character;
+    ptr = fopen("heavenly.txt","r");
+//     if(ptr == NULL){
+//         printf("the file doesnt exist");
+//     }
+//     else{
+//    char c = fgetc(ptr);
+//     while( c != EOF){
+//         printf("%c",c);
+//         c = fgetc(ptr);
+
+//     }
+//     }
+
+    //writing a character in a file
+    ptr = fopen("godly.txt","w");
+    for(int i = 0; i<12;i++){
+        fputc('g',ptr);
+        fputc('o',ptr);
+        fputc('d',ptr);
+        fputc(' ',ptr);
+
+    }
+
+
+
+    return 0;
+}
