e32187ac06455567a30bed34120f8d73bc727d12
Dhruv Roy <dhruvakr9001@gmail.com>
2024-04-29 16:27:48 +0530

    appending in file

---
+++ appendingFile.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    FILE *ptr;
+    ptr = fopen("appendMe.txt", "a"); //append cmd 'a' creates a file if it doesnt exist
+    if (ptr == NULL)
+    {
+        printf("file doesnt exist");
+    }
+    else
+    {
+        fprintf(ptr, "I am getting appended...");
+    }
+    return 0;
+
+
+}
+
